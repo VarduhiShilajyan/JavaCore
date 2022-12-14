2c8392aec1ef6e1514aeb754d845da4ee421df8a
VarduhiShilajyan <vshilajyan@gmail.com>
2022-12-14 23:03:40 +0400

    Homework2

--- src/Homework/Homework1.java
+++ src/Homework/Homework1.java
@@ -1,6 +1,6 @@
-package HomeWork;
+package Homework;
 
-public class HomeWork1 {
+public class Homework1 {
     public static void main(String[] args) {
         System.out.println("Hello from Homework");
     }
---
+++ src/Homework/Homework2.java
@@ -0,0 +1,7 @@
+package Homework;
+
+public class Homework2 {
+    public static void main(String[] args) {
+        System.out.println(" տնային աշխատանք");
+    }
+}
