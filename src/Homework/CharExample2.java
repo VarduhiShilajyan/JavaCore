0f0751a142c2b38964c180844cb8e6752b73404b
VarduhiShilajyan <vshilajyan@gmail.com>
2022-12-19 22:23:46 +0400

    a=b

---
+++ src/Homework/CharExample2.java
@@ -0,0 +1,15 @@
+package Homework;
+
+public class CharExample2 {
+    public static void main(String[] args) {
+        int a = 15;
+        int b = 25;
+        if (a <= b) {
+            System.out.println(" a հավասար է b ");
+        }
+        if (a != b) {
+            System.out.println(" a հավասար չէ b");
+        }
+
+    }
+}
