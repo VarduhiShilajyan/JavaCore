d17a4b238b67cf979176ecd6862498da01e0036e
VarduhiShilajyan <vshilajyan@gmail.com>
2022-12-19 21:02:12 +0400

    տպել 5-10

---
+++ src/Homework/CharExample1.java
@@ -0,0 +1,15 @@
+package Homework;
+
+import java.sql.SQLOutput;
+
+public class CharExample1 {
+    public static void main(String[] args) {
+
+        char a, b;
+        a = 42;
+        b = 'Y';{
+            System.out.println( " a "  +a );
+            System.out.println(  " b " + b);
+        }
+    }
+}
