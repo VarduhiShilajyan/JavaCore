2c869dc880a5b909660104f22fcad6ea35e6ff49
VarduhiShilajyan <vshilajyan@gmail.com>
2022-12-19 20:47:58 +0400

    տպել 5-10

--- src/Homework/IfForExample2.java
+++ src/Homework/IfForExample2.java
@@ -2,11 +2,11 @@ package Homework;
 
 public class IfForExample2 {
     public static void main(String[] args) {
-        int x=5;
-        int y=10;
-        for (x=5; x<=y; x=x+1)
-            System.out.println( " տպել " +x);
-
+        int x = 5;
+        int y = 10;
+        for (x = 5; x <= y; x = x + 1) {
+            System.out.println(" տպել " + x);
+        }
 
     }
 }
