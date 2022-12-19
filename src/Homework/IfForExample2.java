657e7c60cdbdd8aa985f8012bec87278214b914d
VarduhiShilajyan <vshilajyan@gmail.com>
2022-12-19 20:46:33 +0400

    տպել 5-10

---
+++ src/Homework/IfForExample2.java
@@ -0,0 +1,12 @@
+package Homework;
+
+public class IfForExample2 {
+    public static void main(String[] args) {
+        int x=5;
+        int y=10;
+        for (x=5; x<=y; x=x+1)
+            System.out.println( " տպել " +x);
+
+
+    }
+}
