aba685603bc9d4b50bdf38d267f5a3bb9806ceed
VarduhiShilajyan <vshilajyan@gmail.com>
2022-12-22 22:49:37 +0400

    տնային աշխատանք 2

---
+++ src/Homework2/ArrayUtil.java
@@ -0,0 +1,14 @@
+package Homework2;
+
+public class ArrayUtil {
+
+    public static void main(String[] args) {
+        int[] array = {1, 13, 5, -10};
+        for (int i = 0; i <= array.length - 1; i++) {
+            System.out.print(array[i] + " ,");
+        }
+        System.out.println( );
+    }
+
+
+}
