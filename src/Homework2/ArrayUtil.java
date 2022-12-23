109cc0e922850c4030686439c57d6199a8aa1c8c
VarduhiShilajyan <vshilajyan@gmail.com>
2022-12-23 20:13:09 +0400

    տնային աշխատանք 2

--- src/Homework2/ArrayUtil.java
+++ src/Homework2/ArrayUtil.java
@@ -5,10 +5,33 @@ public class ArrayUtil {
     public static void main(String[] args) {
         int[] array = {1, 13, 5, -10};
         for (int i = 0; i <= array.length - 1; i++) {
-            System.out.print(array[i] + " ,");
+            System.out.print(array[i] + " ; ");
         }
-        System.out.println( );
+        System.out.println();
+
+        int max = array[0];
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > max) {
+                max = array[i];
+                {
+                    System.out.println(max);
+                }
+            }
+
+        }
+        int min = array[0];
+        for (int i = array[0]; i < array.length; i++) {
+            if (array[i] < min) {
+                min = array[i];
+                System.out.println(min);
+            }
+
+        }
+
     }
-
-
 }
+
+
+
+
+
