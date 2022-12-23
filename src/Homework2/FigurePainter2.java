6363d22caf1a819ac259730b0ff42483edd33f2c
VarduhiShilajyan <vshilajyan@gmail.com>
2022-12-23 19:15:38 +0400

    տնային աշխատանք 2

--- src/Homework2/FigurePainter2.java
+++ src/Homework2/FigurePainter2.java
@@ -3,35 +3,35 @@ package Homework2;
 public class FigurePainter2 {
     public static void main(String[] args) {
         for (int i = 0; i < 4; i++) {
-            for (int j = 4; j > i; j--) {
+            for (int j = 4; j > i ; j--) {
                 System.out.print("  ");
             }
             for (int j = 0; j <= i; j++) {
-                System.out.print(" *  ");
-            }
-            System.out.println();
-        }
-        for (int i = 0; i <4; i++) {
-            for (int j = 0; j < i+1; j++) {
-                System.out.print("   ");
-            }
-            for (int j = 3; j >i; j--) {
                 System.out.print("  * ");
             }
             System.out.println();
         }
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j <= i+1 ; j++) {
+        System.out.print("  ");
+        }
+        for (int j = 3; j > i; j--) {
+        System.out.print("  * ");
+        }
+        System.out.println();
+        }
         System.out.println();
         for (int i = 0; i <= 4; i++) {
-            for (int j = 0; j < i; j++) {
-                System.out.print("   ");
-            }
-            for (int j = 4; j >= i; j--) {
-                System.out.print("  *");
-            }
-            System.out.println( );
+        for (int j = 0; j < i; j++) {
+        System.out.print("   ");
+        }
+        for (int j = 4; j >= i; j--) {
+        System.out.print("  *");
+        }
+        System.out.println();
         }
 
-    }
+        }
 
-}
+        }
 
