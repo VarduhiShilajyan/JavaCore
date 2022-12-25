e692f7f53167bbd06f06a34a4ed21028589731f7
VarduhiShilajyan <vshilajyan@gmail.com>
2022-12-25 18:29:00 +0400

    տնային աշխատանք 3

--- src/Homework2/ArrayUtil.java
+++ src/Homework2/ArrayUtil.java
@@ -3,33 +3,104 @@ package Homework2;
 public class ArrayUtil {
 
     public static void main(String[] args) {
-        int[] array = {1, 13, 5, -10};
-        for (int i = 0; i <= array.length - 1; i++) {
-            System.out.print(array[i] + " ; ");
+        int[] array = {1, 13, 5, -10, 20};
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+            if (i != array.length - 1) {
+                System.out.print(",");
+            }
         }
         System.out.println();
-
+        // տպել մեծ թիվը
         int max = array[0];
         for (int i = 0; i < array.length; i++) {
             if (array[i] > max) {
                 max = array[i];
-                {
-                    System.out.println(max);
-                }
             }
-
         }
+        System.out.println("max: " + max);
+
         int min = array[0];
-        for (int i = array[0]; i < array.length; i++) {
+        for (int i = 0; i < array.length; i++) {
             if (array[i] < min) {
                 min = array[i];
-                System.out.println(min);
+            }
+        }
+        System.out.println("min: " + min);
+
+        System.out.println();
+
+        System.out.println("second part");
+
+        System.out.println();
+
+        System.out.println("first element is " + array[0]);
+
+        int lastIndex = array.length - 1;
+        System.out.println("last elemen is " + array[lastIndex]);
+
+        System.out.println();
+
+        //տպել զույգ էլեմենտները
+
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                System.out.println(array[i]);
             }
 
         }
+        System.out.println();
+
+        // տպել կենտ էլեմենտները
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 1) {
+                System.out.println(array[i]);
+            }
+        }
+
+
+        System.out.println();
+
+        // տպել զույգերի քնակը
+
+        int y = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 0) {
+                y++;
+            }
+
+        }
+        System.out.println("զույգ թվերի քնակը " + y);
+
+        System.out.println();
+
+        //տպել կենտ թվերի քնակը
+
+        int x = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] % 2 == 1) {
+                x++;
+            }
+
+        }
+        System.out.println("կենտ թվերի քնակն է " + x);
+
+        System.out.println();
+
 
     }
+
+
 }
+
+
+
+
+
+
+
+
 
 
 
