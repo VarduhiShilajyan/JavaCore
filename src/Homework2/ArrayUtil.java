02b9cda0093c9a67de5b8da70f7e930193695289
VarduhiShilajyan <vshilajyan@gmail.com>
2022-12-26 23:41:23 +0400

    տնային աշխատանք 3,  մասիվի միջին թվաբանական հաշվել

--- src/Homework2/ArrayUtil.java
+++ src/Homework2/ArrayUtil.java
@@ -53,6 +53,7 @@ public class ArrayUtil {
         System.out.println();
 
         // տպել կենտ էլեմենտները
+
         for (int i = 0; i < array.length; i++) {
             if (array[i] % 2 == 1) {
                 System.out.println(array[i]);
@@ -71,7 +72,7 @@ public class ArrayUtil {
             }
 
         }
-        System.out.println("զույգ թվերի քանակը " + y);
+        System.out.println("even numbers is  " + y);
 
         System.out.println();
 
@@ -84,14 +85,32 @@ public class ArrayUtil {
             }
 
         }
-        System.out.println("կենտ թվերի քանակն է " + x);
+        System.out.println("odd numbers is " + x);
 
         System.out.println();
 
-        int HalfIndex= array.length/2;
+        int HalfIndex = array.length / 2;
         {
-            System.out.println("half element is" + array[HalfIndex]);
+            System.out.println("half element is " + array[HalfIndex]);
         }
+
+        System.out.println();
+
+        //մասիվի միջին թվաբանական
+
+        double nums[] = {1, 13, 5, -10, 20};
+        double result = 0;
+        for (int i = 0; i < array.length; i++)
+            result = result + nums[i];
+        {
+            System.out.println("the arithmetic mean is equal to " + result / array.length);
+        }
+
+        System.out.println( );
+
+
+
+
     }
 
 
