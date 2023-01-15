6b45b3cbe6e77bd95bfed45292f835d7f43685e3
ProBook <vshilajyan@gmail.com>
2023-01-15 07:04:42 +0400

    Calculator ughghumner katarac

--- src/Homework7/CalculatorDemo.java
+++ src/Homework7/CalculatorDemo.java
@@ -2,14 +2,19 @@ package Homework7;
 
 public class CalculatorDemo {
     public static void main(String[] args) {
-        int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9,};
-        Calculator au = new Calculator();
-        au.plusOfArray(array);
-        au.multiplyOfArray(array);
-        au.minusOfArray(array);
-        au.divideOfArray(array);
-        au.minus1OfArray(array);
-        au.divide1OfArray(array);
+        //int[] array = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9,};
+       // Calculator au = new Calculator();
+       // au.plusOfArray(array);
+       // au.multiplyOfArray(array);
+       // au.minusOfArray(array);
+       // au.divideOfArray(array);
+       // au.minus1OfArray(array);
+       // au.divide1OfArray(array);
+         Calculator cl= new Calculator();
+         cl.plusOfCalculator(30, 20);
+         cl.multiplyOfCalculator(10,1);
+         cl.multiplyOfCalculator(10 , 30);
+         cl.divideOfCalculator(10, 5);
 
     }
 }
